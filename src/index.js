e0b67586efd0ba9c214a13527fe8524219262777
HaNguyenGithub <270297vh@gmail.com>
2024-04-13 10:15:13 +0700

    add-export-function

--- src/index.js
+++ src/index.js
@@ -49,3 +49,82 @@ function exportInvoice() {
     });
   }
   exportInvoice()
+//   async exportDraftBilling(billingId: string): Promise<string> {
+//     try {
+//       const billing = await this.repositories.billing.findUnique({
+//         where: { id: billingId },
+//         include: { billingDetails: true },
+//       })
+
+//       // Get tax value from eclaim
+//       const taxFromEclaim = await this.eclaimService.listTaxTypes('INVOICE')
+//       const tax = taxFromEclaim.find((e) => e.type === billing.taxType)
+
+//       // Read template file synchronously
+//       const data = fs.readFileSync(
+//         join(process.cwd(), 'template/invoice', 'draft-invoice-export.xlsx')
+//       )
+
+//       // Create a template
+//       const template = new XlsxTemplate(data)
+//       // Replacements take place on first sheet
+//       const sheetNumber = 1
+
+//       // Set up some placeholder values matching the placeholders in the template
+//       const values = {
+//         legalName: billing.name,
+//         vatAddress: billing.address,
+//         unitPrice: billing.billingDetails[0].unitPrice,
+//         taxCode: billing.taxCode,
+//         description: billing.billingDetails[0].description,
+//         amount: this.billAmount(
+//           billing.billingDetails[0].unitPrice,
+//           billing.billingDetails[0].quantity
+//         ),
+//         tax: tax.value,
+//         vatTax: this.calculateVat(
+//           this.billAmount(billing.billingDetails[0].unitPrice, billing.billingDetails[0].quantity),
+//           tax.value
+//         ),
+//         totalPayable: this.calculateTotalAmount(
+//           billing.billingDetails[0].unitPrice,
+//           billing.billingDetails[0].unitPrice,
+//           tax.value
+//         ),
+//         dateInvoice: formatDateWithSlashes(new Date()),
+//       }
+
+//       // Perform substitution
+//       template.substitute(sheetNumber, values)
+//       // Get binary data
+//       const filledExcelData = template.generate({ type: 'nodebuffer' })
+
+//       // Đường dẫn tới tệp Excel mới
+//       const currentDate = new Date()
+//       const formattedDate = currentDate.toISOString().slice(0, 19).replace(/[-T:]/g, '_')
+//       const fileName = `draft-invoice-export-${formattedDate}.xlsx`
+//       // const newExcelPath = path.join(process.cwd(), 'template/invoice', newExcelFileName)
+
+//       // Write filled Excel data to new file
+//       // fs.writeFileSync(newExcelPath, filledExcelData, 'binary')
+
+//       // console.log('Đã tạo thành công tệp Excel mới:', newExcelPath)
+
+//       const url = (
+//         await this.fileStorageService.uploadFileFromMail(
+//           [
+//             {
+//               fileBuffer: filledExcelData,
+//               contentType: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+//               filename: fileName,
+//             },
+//           ],
+//           false
+//         )
+//       ).toString()
+//       console.log('url:', url)
+//       return url
+//     } catch (err) {
+//       console.log('err.....:', err)
+//     }
+//   }
